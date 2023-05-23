b7b31b618c5c08742f721f53760af1e830393e1d
programmer-88 <ianmbayyz@gmail.com>
2023-05-23 16:32:22 +0300

    putchar

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * int _isalpha(int c);
+ * Description: using isalpha to find alphabet
+ * @c: variable to find alphabet
  * Return: 1 if c is a letter,lowercase or uppercase & 0 otherwise
  */
 int _isalpha(int c)
