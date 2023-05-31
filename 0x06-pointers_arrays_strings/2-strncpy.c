ea3f06ce29ce704761e760c2858bb9e5c0302617
programmer-88 <ianmbayyz@gmail.com>
2023-05-31 23:09:30 +0300

    strcat

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of int
+ * Return: 0 for success
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	n = strncpy(char *dest, const char *src, size_t n);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@
 #include <stdlib.h>
 
 char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
