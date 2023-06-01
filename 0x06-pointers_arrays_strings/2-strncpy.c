07b3ba3fe4fef9834df49ce41fc7d4ab0237e5c7
programmer-88 <ianmbayyz@gmail.com>
2023-06-02 00:09:38 +0300

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,5 +10,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	return  strncpy(char *dest, const char *src, size_t n);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
