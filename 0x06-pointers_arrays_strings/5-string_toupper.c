bbced19ea468210a706ccf7715d6cab051d84fd0
programmer-88 <ianmbayyz@gmail.com>
2023-06-02 00:59:07 +0300

    Always LOOK UP

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,11 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; str[i]!='\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if(str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] = str[i] -32;
+			str[i] = str[i] - 32;
 		}
 	}
 	return (str);
