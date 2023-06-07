1ba6ce05d341b2ca1ff2ee45a3cf0130622feaa6
programmer-88 <ianmbayyz@gmail.com>
2023-06-07 12:11:21 +0300

    readme

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number using putchar
+ * @n: integer to be printes
+ * Return: is void
+ */
+
+void print_number(int n)
+{
+	putchar('0' + n);
+}
