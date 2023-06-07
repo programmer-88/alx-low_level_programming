d452ad4d301d818feb945a23984c55dad6150f1e
programmer-88 <ianmbayyz@gmail.com>
2023-06-07 14:58:58 +0300

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
