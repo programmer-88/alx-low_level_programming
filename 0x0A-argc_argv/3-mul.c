f87a16f463a66510d2cd1d49273d83355fbb6be9
programmer-88 <ianmbayyz@gmail.com>
2023-06-09 16:28:19 +0300

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - multiplies two arguments passed to main
+ *
+ * @argc: argument count
+ * @argv: argument value
+ * Return: product or 1 "and error" if arguments are no arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, prod = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			prod *= atoi(argv[i]);
+		}
+	printf("%d\n", prod);
+	return (0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}
