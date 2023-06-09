0ef42e130a13c6ca01e4506e53a36b62268c7fd0
programmer-88 <ianmbayyz@gmail.com>
2023-06-09 13:17:08 +0300

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints name of programe
+ *
+ * @argcUnused: argument count(it is unused)
+ * @argv: argument value
+ * Return: 0
+ */
+
+int main(int argcUnused __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
