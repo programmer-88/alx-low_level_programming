44781b1a092c08e57c7e393461d38e24f32100ac
programmer-88 <ianmbayyz@gmail.com>
2023-06-20 01:11:49 +0300

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 
 /**
@@ -17,5 +18,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
