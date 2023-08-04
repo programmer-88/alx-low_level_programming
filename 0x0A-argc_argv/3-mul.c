7fd8d09d5e63f38641ece50867ddcb0fccbf54ca
programmer-88 <ianmbayyz@gmail.com>
2023-08-05 00:15:02 +0300

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,17 @@
 */
 int main(int argc, char *argv[])
 {
-	int i = 0;
-	
-	while (i < argc)
+	int j, k;
+
+	if (argc != 3)
 	{
-		printf("%s\n", argv[i]);
-		i++;
+		printf("Error\n");
+		return (1);
 	}
+	j = atoi(argv[1]);
+	k = atoi(argv[2]);
+
+	printf("%d\n", j * k);
 
 	return (0);
 }
