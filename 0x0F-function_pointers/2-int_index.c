8dccccf8994fe3fc6294aed20b31b6c4b1e028ef
programmer-88 <ianmbayyz@gmail.com>
2023-08-17 01:37:11 +0300

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an index
+ * @array: array
+ * @size: size of array
+ * @cmp: index of first element
+ * Return: -1 if it finds nothing otherwise cmp
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i] != 0))
+		{
+			return (i);
+		}
+
+	}
+	return (-1);
+
+}
