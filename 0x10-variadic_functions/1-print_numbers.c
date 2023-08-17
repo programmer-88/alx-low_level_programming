c2e0a6f01eef17ec55009732542d37d36d6d4423
programmer-88 <ianmbayyz@gmail.com>
2023-08-18 00:30:10 +0300

    To be it to be the value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		x =  va_arg(printnum, int);
 		printf("%d", x);
-		if (i != (n -1) && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
