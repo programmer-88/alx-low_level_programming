ccb07f25707bf00a8b474860fe3b76428c65c427
programmer-88 <ianmbayyz@gmail.com>
2023-08-30 00:40:57 +0300

    10 C alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10x
+ * Return: is void
+*/
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
