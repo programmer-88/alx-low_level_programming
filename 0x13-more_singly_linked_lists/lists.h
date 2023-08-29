73cf0ea0a138c4e34b7cd5c97f6c761d1bcf2d9b
programmer-88 <ianmbayyz@gmail.com>
2023-08-29 14:04:08 +0300

    Print list

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: number
+ * @next: next node
+*/
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+#endif
