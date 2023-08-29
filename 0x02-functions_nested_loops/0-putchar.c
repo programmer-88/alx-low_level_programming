fb3662cdd8f9e3d62365808fb32dd99616e1b1e1
programmer-88 <ianmbayyz@gmail.com>
2023-08-30 00:13:34 +0300

    _puthcar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - print putchar
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-    char c[] = "_putchar";
-    int i = 0;
+	char c[] = "_putchar";
+	int i = 0;
 
-    while (i < 8)
-    {
-        putchar(c[i]);
-        i++;
-    }
-    putchar('\n');
-    return (0);
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
