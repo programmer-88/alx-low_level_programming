5960f047155b7fefad4cf9c2e224b97ba2b6eed5
programmer-88 <ianmbayyz@gmail.com>
2023-08-31 19:17:43 +0300

    binary_to_unint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,10 +16,17 @@ unsigned int binary_to_uint(const char *b)
 
 	len = strlen(b);
 
+	if (b == NULL)
+	{
+		return (0);
+	}
+	
 	for (c = len - 1; c >= 0; c--)
 	{
 		if (b[c] != '0' && b[c] != '1')
-		return (0);
+		{
+			return (0);
+		}
 		if (b[c] == '1')
 		{
 			i += k;
