ccfab2b112229aebd2099698d866866a1ef85874
programmer-88 <ianmbayyz@gmail.com>
2023-09-05 01:16:00 +0300

    Speak hently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,30 +9,30 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	FILE *file;
+	int op, wr, l = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	if (text_content == NULL)
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l];)
+		{
+			l++;
+		}
+
+	}
+
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, l);
+
+	if (op == -1 || wr == -1)
 	{
 		return (-1);
 	}
-	file = fopen(filename, "a");
 
-	if (file == NULL)
-	{
-		return (-1);
-	}
-
-	if (fputs(text_content, file) == EOF)
-	{
-		fclose(file);
-		return (-1);
-	}
-
-	fclose(file);
+	close(op);
 	return (1);
 }
