2df77a44c24fc843a26543a96e990561701575f1
programmer-88 <ianmbayyz@gmail.com>
2023-09-05 00:31:50 +0300

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,33 +9,28 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytes_wr;
+	int op, wr, len = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC,  0600);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+		{
+			len++;
+		}
+	}
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(op, text_content, len);
 
-	if (fd == -1)
+	if (op == -1 || wr == -1)
 	{
 		return (-1);
 	}
 
-	if (text_content != NULL)
-	{
-		bytes_wr = write(fd, text_content, strlen(text_content));
-
-		if (bytes_wr == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-
-	}
-
-	close(fd);
-	return (-1);
+	close(op);
+	return (1);
 }
